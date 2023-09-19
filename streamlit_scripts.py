eca90dabe7886a423fe380a9f1c9af6837b26057
Jinhee <75326467+joyheeh@users.noreply.github.com>
2023-09-19 10:23:05 +0900

    file name updated

--- streamlit_scripts.py
+++ streamlit_scripts.py
@@ -4,7 +4,7 @@ import numpy as np
 from datetime import datetime, timedelta
 # import webbrowser
 
-my_katalk_df = pd.read_csv("preprocessed_data/" +  "challenge_msg_2023-09-17.csv")
+my_katalk_df = pd.read_csv("preprocessed_data/" +  "challenge_msg_2023-09-18.csv")
 
 challenge_url_df = my_katalk_df[my_katalk_df['text'].str.contains('https://cafe.naver.com/roalnam/')]
 challenge_url_count_df = challenge_url_df[['year_month_day','user_name','text', 'user_class']]
